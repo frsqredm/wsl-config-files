de0833834e16dee98d85f56bc7a7e6c21dcf65c6
frsqredm <fr.sqre.dm@gmail.com>
2024-07-13 10:56:42 +0700

    u: clean neovim directory

--- neovim/options.lua
+++
@@ -1,4 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.g.ruby_host_prog = "$HOME/.asdf/installs/ruby/3.3.3/bin/neovim-ruby-host"
