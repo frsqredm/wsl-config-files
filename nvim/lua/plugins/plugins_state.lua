64d272aa73e4988ba87c71f2eccf165bc0983786
frsqredm <fr.sqre.dm@gmail.com>
2024-07-14 00:02:32 +0700

    a: add neovim config (LazyVim)

---
+++ nvim/lua/plugins/plugins_state.lua
@@ -0,0 +1,7 @@
+return {
+  --enable
+  { "j-hui/fidget.nvim", tag = "v1.4.5", opts = {} },
+  --disable
+  { "folke/noice.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+}
